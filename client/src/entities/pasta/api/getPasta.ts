8e8c974fc4b331d5b5704b431802cfbe44a46b37
Sayolight <sayolight@proton.me>
2026-03-04 16:46:27 +0100

    feat(pasta): search input

--- client/src/entities/pasta/api/getPasta.ts
+++ client/src/entities/pasta/api/getPasta.ts
@@ -2,6 +2,10 @@ import type { AxiosResponse } from "axios";
 import { http } from "@/shared/api/https.ts";
 import type { Pasta } from "@/entities/pasta";
 
-export function getPasta(): Promise<AxiosResponse<Pasta[]>> {
-  return http.get(`/pasta`);
+export function getPasta(query?: string): Promise<AxiosResponse<Pasta[]>> {
+  return http.get(`/pasta`, {
+    params: {
+      query: query,
+    },
+  });
 }
--- client/src/pages/Gallery/GalleryPage.tsx
+++ client/src/pages/Gallery/GalleryPage.tsx
@@ -2,17 +2,22 @@ import { Block } from "@ui/block";
 import PastaBoard from "@/widgets/pasta-board/ui/PastaBoard.tsx";
 import { Button } from "@ui/button";
 import { CreatePastaForm } from "@/features/pasta/create/ui/CreatePastaForm.tsx";
-import { useEffect, useState } from "react";
+import { type ChangeEvent, useEffect, useState } from "react";
 import { getPasta } from "@/entities/pasta/api/getPasta.ts";
 import type { Pasta } from "@/entities/pasta";
+import { Input } from "@ui/input";
 
 export default function GalleryPage() {
   const [isOpen, setIsOpen] = useState(false);
   const [pasta, setPasta] = useState<Pasta[]>([]);
 
-  const fetchPasta = async () => {
-    const res = await getPasta();
+  const fetchPasta = async (query?: string) => {
+    const res = await getPasta(query);
     setPasta(res.data);
+  };
+
+  const search = (e: ChangeEvent<HTMLInputElement>) => {
+    fetchPasta(e.target.value).then();
   };
 
   useEffect(() => {
@@ -21,9 +26,16 @@ export default function GalleryPage() {
 
   return (
     <Block isCard={false}>
-      <Button variant={"primary"} onClick={() => setIsOpen(true)}>
-        + create a new pasta
-      </Button>
+      <Block>
+        <Input
+          title={"search"}
+          placeholder={"where is my funny gif with monkeys..."}
+          onChange={search}
+        ></Input>
+        <Button variant={"primary"} onClick={() => setIsOpen(true)}>
+          + create a new pasta
+        </Button>
+      </Block>
       <PastaBoard pasta={pasta} />
       <CreatePastaForm
         isOpen={isOpen}
