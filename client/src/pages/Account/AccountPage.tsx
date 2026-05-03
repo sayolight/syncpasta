ff4585b69f1fabae2a431afb873cb922613a70c4
sayolight <sayolight@proton.me>
2026-05-03 15:25:27 +0200

    chore(account-page): remove "account info" block

--- client/src/pages/Account/AccountPage.tsx
+++ client/src/pages/Account/AccountPage.tsx
@@ -1,5 +1,4 @@
 import { Block } from "@ui/block";
-import { Typography } from "@/shared/ui";
 import PasswordResetButton from "@/widgets/password-reset/ui/PaswordResetButton.tsx";
 import EmailUpdateForm from "@/widgets/email-update/ui/EmailUpdateForm.tsx";
 import { SignOutButton } from "@/features/auth/sign-out/ui/SignOutButton.tsx";
@@ -7,10 +6,6 @@ import { SignOutButton } from "@/features/auth/sign-out/ui/SignOutButton.tsx";
 export default function AccountPage() {
   return (
     <Block isCard={false}>
-      <Block title="account info">
-        <Typography>hi, user!</Typography>
-        <Typography variant={"muted"}>you have totally 361 pastas</Typography>
-      </Block>
       <Block title="change email">
         <EmailUpdateForm />
       </Block>
