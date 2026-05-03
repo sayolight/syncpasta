20db5e2d31ccd4d489890ee9d75e12340e844378
sayolight <sayolight@proton.me>
2026-05-03 15:24:24 +0200

    feat(header): hide auth-protected routes if user isn't authed

--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -1,7 +1,20 @@
 import { Link, useLocation } from "react-router";
+import { useEffect, useState } from "react";
+import { getAuth } from "@/shared/api/firebase";
+import type { User } from "firebase/auth";
 
 export default function Header() {
   const location = useLocation();
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    const auth = getAuth();
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      setUser(user);
+    });
+
+    return () => unsubscribe();
+  }, []);
 
   return (
     <header className="header">
@@ -13,9 +26,15 @@ export default function Header() {
         </span>
       </Link>
       <div className="header__nav">
-        <Link to={{ pathname: "/applications" }}>applications</Link>
-        <Link to={{ pathname: "/gallery" }}>gallery</Link>
-        <Link to={{ pathname: "/auth" }}>account</Link>
+        {user ? (
+          <>
+            <Link to={{ pathname: "/applications" }}>applications</Link>
+            <Link to={{ pathname: "/gallery" }}>gallery</Link>
+            <Link to={{ pathname: "/account" }}>account</Link>
+          </>
+        ) : (
+          <Link to={{ pathname: "/auth" }}>auth</Link>
+        )}
       </div>
     </header>
   );
