913e065e7d49a531504b6c8153850b82ed2f62b2
sayolight <sayolight@proton.me>
2026-05-03 15:31:45 +0200

    chore(email-update): redirect to "/auth" when "auth/requires-recent-login"

--- client/src/features/auth/email-update/model/useEmailUpdate.ts
+++ client/src/features/auth/email-update/model/useEmailUpdate.ts
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import * as firebase from "@/shared/api/firebase";
 import { FirebaseError } from "@firebase/app";
+import { useNavigate } from "react-router";
 
 export function useEmailUpdate() {
   const [isLoading, setLoading] = useState(false);
   const [error, setError] = useState<string | null>();
   const [success, setSuccess] = useState<boolean>(false);
+  const navigate = useNavigate();
 
   const emailUpdate = async (email: string) => {
     try {
@@ -16,6 +18,10 @@ export function useEmailUpdate() {
       setSuccess(true);
     } catch (e) {
       if (e instanceof FirebaseError) {
+        if (e.code === "auth/requires-recent-login") {
+          navigate("/auth?redirect_to=account");
+          // TODO: return a "you should re-auth to update email" message to user
+        }
         setError(e.code);
       }
     } finally {
