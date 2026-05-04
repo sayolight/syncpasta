5b78d482d57d156475100d30b36fa68eee9dc2cc
sayolight <sayolight@proton.me>
2026-05-04 14:24:43 +0200

    feat(i18n): add i18n module to app

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,5 +1,6 @@
 import { RouterProvider } from "react-router";
 import router from "@/app/router.tsx";
+import "@/app/i18n";
 
 function App() {
   return <RouterProvider router={router} />;
---
+++ client/src/app/i18n.ts
@@ -0,0 +1,18 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import I18nextBrowserLanguageDetector from "i18next-browser-languagedetector";
+import Backend from "i18next-http-backend";
+
+i18n
+  .use(Backend)
+  .use(I18nextBrowserLanguageDetector)
+  .use(initReactI18next)
+  .init({
+    backend: {
+      loadPath: "/locales/{{lng}}.json",
+    },
+    fallbackLng: "en",
+  })
+  .then(() => {});
+
+export default i18n;
