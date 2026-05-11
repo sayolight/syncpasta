40cc4a2c0712b9bd0521f1c8bebfcc3ffc610b53
sayolight <sayolight@proton.me>
2026-05-11 19:15:31 +0200

    refactor(auth): make sign in requests with tanstackquery

--- client/src/features/auth/sign-in/model/useSignIn.ts
+++ client/src/features/auth/sign-in/model/useSignIn.ts
@@ -1,27 +1,12 @@
-import { useState } from "react";
 import * as firebase from "@/shared/api/firebase";
-import { FirebaseError } from "@firebase/app";
-import { useNavigate } from "react-router";
+import { useMutation } from "@tanstack/react-query";
+
+async function signIn(data: { email: string; password: string }) {
+  await firebase.signIn(data.email, data.password);
+}
 
 export function useSignIn() {
-  const [isLoading, setLoading] = useState(false);
-  const navigate = useNavigate();
-  const [error, setError] = useState<string | null>();
-
-  const signIn = async (data: { email: string; password: string }) => {
-    try {
-      setLoading(true);
-      await firebase.signIn(data.email, data.password);
-      setError(null);
-      navigate("/");
-    } catch (e) {
-      if (e instanceof FirebaseError) {
-        setError(e.code);
-      }
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  return { signIn, isLoading, error };
+  return useMutation({
+    mutationFn: signIn,
+  });
 }
--- client/src/features/auth/sign-in/ui/SignInForm.tsx
+++ client/src/features/auth/sign-in/ui/SignInForm.tsx
@@ -4,30 +4,40 @@ import { Button } from "@ui/button";
 import { useSignIn } from "@/features/auth/sign-in/model/useSignIn.ts";
 import { useState } from "react";
 import * as React from "react";
-import { Alert } from "@/shared/ui";
 import { useTranslation } from "react-i18next";
+import { useNavigate } from "react-router";
+import { ErrorAlert } from "@/widgets/error-alert/ui/ErrorAlert.tsx";
 
 export function SignInForm() {
+  const navigate = useNavigate();
   const { t } = useTranslation();
-  const { signIn, isLoading, error } = useSignIn();
+  const { error, isPending, mutate } = useSignIn();
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   const onSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-    await signIn({ email, password });
+    mutate(
+      { email, password },
+      {
+        onSuccess: async () => {
+          navigate("/");
+        },
+      },
+    );
   };
 
   return (
     <>
       <form onSubmit={onSubmit}>
         <Block title={t("auth.sign.in.title")}>
-          {error && <Alert title={"⚠ error!"}>{error}</Alert>}
+          <ErrorAlert error={error} />
           <Input
             title={t("auth.sign.email.title")}
             type={"email"}
             value={email}
+            disabled={isPending}
             onChange={(e) => setEmail(e.target.value)}
             placeholder={t("auth.sign.email.placeholder")}
           ></Input>
@@ -35,10 +45,11 @@ export function SignInForm() {
             title={t("auth.sign.password.title")}
             type={"password"}
             value={password}
+            disabled={isPending}
             onChange={(e) => setPassword(e.target.value)}
             placeholder={t("auth.sign.password.placeholder")}
           ></Input>
-          <Button disabled={isLoading} type="submit">
+          <Button disabled={isPending} type="submit">
             {t("auth.sign.in.title")}
           </Button>
         </Block>
