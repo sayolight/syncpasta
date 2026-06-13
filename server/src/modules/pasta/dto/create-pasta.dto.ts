fafe2d74913f2b0bf134a20bb35d793ab9fd5703
sayolight <sayolight@proton.me>
2026-06-13 15:08:44 +0200

    docs(pasta-dto): add pasta-dto params to swagger schemas

--- server/src/modules/pasta/dto/create-pasta.dto.ts
+++ server/src/modules/pasta/dto/create-pasta.dto.ts
@@ -1,9 +1,12 @@
 import { IsNotEmpty, IsOptional } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreatePastaDto {
+  @ApiProperty()
   @IsNotEmpty()
   keywords: string;
 
+  @ApiProperty()
   @IsOptional()
   text?: string;
 }
--- server/src/modules/pasta/dto/update-pasta.dto.ts
+++ server/src/modules/pasta/dto/update-pasta.dto.ts
@@ -1,4 +1,4 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { PartialType } from '@nestjs/swagger';
 import { CreatePastaDto } from './create-pasta.dto';
 
 export class UpdatePastaDto extends PartialType(CreatePastaDto) {}
