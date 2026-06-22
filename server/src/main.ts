6fedbb691f74bf331bd5e1be2dd5f8b9e7753af3
sayolight <sayolight@proton.me>
2026-06-22 21:49:21 +0200

    feat(cors): enable cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,7 +4,6 @@ import { BadRequestException, ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { TransformInterceptor } from './core/response/transform.interceptor';
 import { HttpExceptionFilter } from './core/response/http-exception.filter';
-import { PastaController } from './modules/pasta/pasta.controller';
 import { PastaModule } from './modules/pasta/pasta.module';
 
 async function bootstrap() {
@@ -12,6 +11,9 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
   app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalInterceptors(new TransformInterceptor());
+  app.enableCors({
+    origin: true,
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       exceptionFactory(errors) {
