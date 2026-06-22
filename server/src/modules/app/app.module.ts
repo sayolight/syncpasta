433da82f57b7586c0efb51d707f4b255985a068f
sayolight <sayolight@proton.me>
2026-06-22 21:49:09 +0200

    feat(rate-limit): add rate-limit module

--- server/src/modules/app/app.module.ts
+++ server/src/modules/app/app.module.ts
@@ -3,8 +3,29 @@ import { CoreModule } from '../../core/core.module';
 import { PastaModule } from '../pasta/pasta.module';
 import { UsersModule } from '../users/users.module';
 import { ApplicationModule } from '../applications/application.module';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-  imports: [CoreModule, PastaModule, UsersModule, ApplicationModule],
+  imports: [
+    ThrottlerModule.forRoot({
+      throttlers: [
+        {
+          ttl: 15000,
+          limit: 100,
+        },
+      ],
+    }),
+    CoreModule,
+    PastaModule,
+    UsersModule,
+    ApplicationModule,
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
